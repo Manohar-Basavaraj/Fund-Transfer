958b2620e2c82563f7855b1b58301eb6d4414397
manohar <manohar.b@softvision.com>
2018-06-01 16:56:47 +0530

    Commit after removing Xalan folder

--- FundTransfer/src/test/java/fundtransfer/test/RunCukesTest.java
+++ FundTransfer/src/test/java/fundtransfer/test/RunCukesTest.java
@@ -5,10 +5,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/",
+@CucumberOptions(
 		plugin = {"pretty", "html:target/report/report.html",
         "json:target/report/cucu_json_report.json",
-        "junit:target/report/cucumber_junit_report.xml","html:target/cucumber","json:target/cucumber.json"})
+        "junit:target/report/cucumber_junit_report.xml"})
 public class RunCukesTest {
 	
 }
