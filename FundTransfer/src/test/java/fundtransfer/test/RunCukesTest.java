1b13d6ccd94fb17762dbe0598c20fd7db0db4704
manohar <manohar.b@softvision.com>
2018-06-01 19:35:47 +0530

    Pradeep after changes to runcukes file

--- FundTransfer/src/test/java/fundtransfer/test/RunCukesTest.java
+++ FundTransfer/src/test/java/fundtransfer/test/RunCukesTest.java
@@ -5,17 +5,11 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-/*@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(
 		plugin = {"pretty", "html:target/report/report.html",
         "json:target/report/cucu_json_report.json",
         "junit:target/report/cucumber_junit_report.xml"})
 public class RunCukesTest {
 	
-}*/
-
-@RunWith(Cucumber .class)
-@CucumberOptions(plugin = {"pretty"})
-public class RunCukesTest {
-
 }
