7a1c6d879ea32ec843a9a2f628aa38ac8cc07b79
manohar <manohar.b@softvision.com>
2018-06-01 14:31:19 +0530

    Added Cucumber Path in the support file

--- FundTransfer/src/test/java/fundtransfer/test/RunCukesTest.java
+++ FundTransfer/src/test/java/fundtransfer/test/RunCukesTest.java
@@ -6,8 +6,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/report/report.html",
+@CucumberOptions(features = "src/test/resources/",
+		plugin = {"pretty", "html:target/report/report.html",
         "json:target/report/cucu_json_report.json",
-        "junit:target/report/cucumber_junit_report.xml"})
+        "junit:target/report/cucumber_junit_report.xml","html:target/cucumber","json:target/cucumber.json"})
 public class RunCukesTest {
+	
 }
